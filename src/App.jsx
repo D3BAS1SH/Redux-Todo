9ae9e5be6c851ae7728d55f7da96c66d8c0981c8
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-19 03:07:33 +0530

    Added useSelector using and handling TODOX.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+
 const App = () => {
 
   return (
     <>
       <h1>This is just a Redux Part</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSelector,useDispatch} from 'react-redux'
+import {removeTodo} from '../features/todo/todoSlice'
 
 const Todos = () => {
+    const todox = useSelector(state => state.todox)
+    const dispatch=useDispatch()
   return (
-    <div>Todos</div>
+    <ul>
+        <div>TODOS</div>
+        {
+            todox.map((todo)=>(
+                <li key={todo.id}>
+                    {todo.text}
+                    <button 
+                    onClick={()=>dispatch(removeTodo(todo.id))}>
+                    X
+                    </button>
+                </li>
+            ))
+        }
+    </ul>
   )
 }
 
